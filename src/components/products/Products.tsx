9f107b8eddc4e81c8c8b494e1b0ffbfbcca87a61
dyda <ivanduda102@gmail.com>
2024-10-22 02:06:49 +0300

    lesson/1.1

---
+++ src/components/products/Products.tsx
@@ -0,0 +1,23 @@
+import React, {FC} from 'react';
+import {IProductModel} from "../models/IProductModel";
+import Product from "../product/Product";
+import {apiProductsObject} from "../../data";
+
+
+
+const Products: FC<any> = () => {
+const products: IProductModel[] = apiProductsObject.products
+
+    return (
+
+        <div>
+            {
+
+                products.map((product: IProductModel) => (<Product key={product.id} item={product}/>))
+
+}
+        </div>
+    );
+}
+
+export default Products;
