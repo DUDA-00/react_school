19ef6c0d7b6f339dbf6c4067aba832ed322dd248
dyda <ivanduda102@gmail.com>
2024-11-21 20:54:27 +0200

    lesson/10

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import './App.css';
+import {useAppDispatch, useAppSelector, userSliceActions} from "./redux/store";
 
 function App() {
+  let {users, user} = useAppSelector(state => state.userPart );
+  let dispatch = useAppDispatch();
+
+  useEffect(()=>{
+    dispatch(userSliceActions.loadUser(1));
+  })
+
   return (
     <div>
+      {users.map((user)=> (<div key={user.id}>{user.username}</div>))}
 
+      {user && <div>{JSON.stringify(user)}</div>}
     </div>
   );
 }
